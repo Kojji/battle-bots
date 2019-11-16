3b22559bb74a49593176b4155323bb2f971b4fb0
kojji <fernandokojiyama@gmail.com>
2019-11-16 19:56:24 -0400

    add: robo class methods and attributes fixed

--- src/main/java/model/Robo.java
+++ src/main/java/model/Robo.java
@@ -10,42 +10,35 @@ package model;
  * @author Fernando Koji
  */
 public class Robo {
-    private String J1;
-    private String J2;
+    private String nome;
     private Arma arma;
     private Arena posicao;
     private int vida;
 
-    public Robo(Arma arma, Arena posicao, int vida) {
-        this.J1 = "Jogador 1";
-        this.J2 = "Jogador 2";
+    public Robo(Arma arma, Arena posicao, int vida, String nome) {
+        this.nome = nome;
         this.arma = arma;
         this.posicao = posicao;
         this.vida = vida;
     }
 
-    public Arma getArma() {
-        return arma;
-    }
 
-    public void setArma(Arma arma) {
-        this.arma = arma;
-    }
+    public String getNome() { return this.nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public Arma getArma() { return this.arma; }
+    public void setArma(Arma arma) { this.arma = arma; }
+    public Arena getPosicao() { return this.posicao; }
+    public void setPosicao(Arena posicao) { this.posicao = posicao; }
+    public int getVida() { return this.vida; }
+    public void setVida(int vida) { this.vida = vida; }
 
-    public Arena getPosicao() {
-        return posicao;
+    public int increaseVida(int value) {
+        this.setVida(this.getVida()+value);
+        return this.getVida();
     }
-
-    public void setPosicao(Arena posicao) {
-        this.posicao = posicao;
-    }
-
-    public int getVida() {
-        return vida;
-    }
-
-    public void setVida(int vida) {
-        this.vida = vida;
+    public int decreaseVida(int value) {
+        this.setVida(this.getVida()-value);
+        return this.getVida();
     }
     
     
